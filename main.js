6be57c99a8d2d8751e10dc24f0e8e6a907e3cef0
Mauro Bono <bono.mauro@gmail.com>
2018-09-13 14:16:50 +0200

    finish portfolio page

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+//mobile menu toggling
 const hamburger = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
+const footer = document.querySelector('.f-mobile');
 
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('open');
+  footer.classList.toggle('f-mobile-disappear');
 });
+
+//dynamic footer year and date
+const footerDate = document.querySelectorAll('.footer-date');
+const date = new Date();
+const year = date.getFullYear();
+footerDate.forEach(item => (item.innerHTML = year));
